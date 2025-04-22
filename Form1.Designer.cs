957fb3cf3ccbe2945f53ecb835857a8de7d50b6a
Edip Mert Eren <mert.eren@visiomex.com>
2025-04-22 11:18:28 +0300

    .

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            txtGuidInput = new TextBox();
+            txtEncryptedOutput = new TextBox();
+            btnEncrypt = new Button();
+            btnCopy = new Button();
+            btnGetGuid = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Courier New", 12F);
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(68, 18);
+            label1.TabIndex = 0;
+            label1.Text = "GUID :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Courier New", 12F);
+            label2.Location = new Point(12, 90);
+            label2.Name = "label2";
+            label2.Size = new Size(88, 18);
+            label2.TabIndex = 1;
+            label2.Text = "OUTPUT :";
+            // 
+            // txtGuidInput
+            // 
+            txtGuidInput.Location = new Point(106, 8);
+            txtGuidInput.Name = "txtGuidInput";
+            txtGuidInput.Size = new Size(350, 23);
+            txtGuidInput.TabIndex = 2;
+            // 
+            // txtEncryptedOutput
+            // 
+            txtEncryptedOutput.Location = new Point(106, 89);
+            txtEncryptedOutput.Name = "txtEncryptedOutput";
+            txtEncryptedOutput.Size = new Size(297, 23);
+            txtEncryptedOutput.TabIndex = 3;
+            // 
+            // btnEncrypt
+            // 
+            btnEncrypt.Location = new Point(355, 37);
+            btnEncrypt.Name = "btnEncrypt";
+            btnEncrypt.Size = new Size(101, 46);
+            btnEncrypt.TabIndex = 4;
+            btnEncrypt.Text = "Encrypt";
+            btnEncrypt.UseVisualStyleBackColor = true;
+            btnEncrypt.Click += btnEncrypt_Click;
+            // 
+            // btnCopy
+            // 
+            btnCopy.Location = new Point(409, 89);
+            btnCopy.Name = "btnCopy";
+            btnCopy.Size = new Size(47, 23);
+            btnCopy.TabIndex = 5;
+            btnCopy.Text = "Copy";
+            btnCopy.UseVisualStyleBackColor = true;
+            btnCopy.Click += btnCopy_Click;
+            // 
+            // btnGetGuid
+            // 
+            btnGetGuid.Location = new Point(248, 37);
+            btnGetGuid.Name = "btnGetGuid";
+            btnGetGuid.Size = new Size(101, 46);
+            btnGetGuid.TabIndex = 6;
+            btnGetGuid.Text = "Get GUID";
+            btnGetGuid.UseVisualStyleBackColor = true;
+            btnGetGuid.Click += btnGetGuid_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(468, 127);
+            Controls.Add(btnGetGuid);
+            Controls.Add(btnCopy);
+            Controls.Add(btnEncrypt);
+            Controls.Add(txtEncryptedOutput);
+            Controls.Add(txtGuidInput);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "License Creator";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtGuidInput;
+        private TextBox txtEncryptedOutput;
+        private Button btnEncrypt;
+        private Button btnCopy;
+        private Button btnGetGuid;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,90 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
 namespace LicenseCreator
 {
     public partial class Form1 : Form
     {
+        private static readonly string gizliSifre = "1MertinGizliAnahtari1";
         public Form1()
         {
             InitializeComponent();
         }
+
+        #region Button Click
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            string guid = txtGuidInput.Text.Trim();
+
+            if (string.IsNullOrEmpty(guid))
+            {
+                MessageBox.Show("Lütfen bir GUID girin.");
+                return;
+            }
+
+            string encrypted = Encrypt(guid, gizliSifre);
+            Console.WriteLine($"Encrypted: {encrypted}");
+            txtEncryptedOutput.Text = encrypted;
+        }
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtEncryptedOutput.Text))
+            {
+                MessageBox.Show("Lütfen önce şifrelenmiş metni oluşturun.");
+                return;
+            }
+            Clipboard.SetText(txtEncryptedOutput.Text);
+            MessageBox.Show("Şifrelenmiş metin panoya kopyalandı.");
+        }
+        private void btnGetGuid_Click(object sender, EventArgs e)
+        {
+            string machineGuid = GetMachineGuid();
+            txtGuidInput.Text = machineGuid;
+        }
+
+        #endregion
+
+        #region Methods
+
+        private string Encrypt(string plainText, string key)
+        {
+            byte[] keyBytes = GetFixedSizeKey(key);
+            Console.WriteLine($"Key: {Convert.ToBase64String(keyBytes)}");
+            byte[] iv = Encoding.UTF8.GetBytes("1234567890abcdef");
+
+            using Aes aes = Aes.Create();
+            aes.Key = keyBytes;
+            aes.IV = iv;
+
+            using MemoryStream ms = new();
+            using CryptoStream cs = new(ms, aes.CreateEncryptor(), CryptoStreamMode.Write);
+            using StreamWriter sw = new(cs);
+            sw.Write(plainText);
+            sw.Close();
+            return Convert.ToBase64String(ms.ToArray());
+        }
+        private byte[] GetFixedSizeKey(string key)
+        {
+            using SHA256 sha = SHA256.Create();
+            return sha.ComputeHash(Encoding.UTF8.GetBytes(key));
+        }
+        private string GetMachineGuid()
+        {
+            using (var key = Microsoft.Win32.Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Cryptography"))
+            {
+                if (key != null)
+                {
+                    object value = key.GetValue("MachineGuid");
+                    if (value != null)
+                    {
+                        return value.ToString();
+                    }
+                }
+            }
+            return string.Empty;
+        }
+
+        #endregion
     }
 }
